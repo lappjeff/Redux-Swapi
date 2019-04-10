06ac5d70e16ce5bb6608b01d51abadb9885ec8f5
unknown <lappjeff20@gmail.com>
2019-04-10 16:22:38 -0400

    Functionality complete, everything working

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,4 +10,18 @@ export const FETCH_PEOPLE_FAIL = 'FETCH_PEOPLE_FAIL'
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
-export
+export const getPeople = () => dispatch => {
+  dispatch({ type: FETCH_PEOPLE_START })
+
+  axios.get(`https://swapi.co/api/people/`)
+    .then(res => dispatch({
+        type: FETCH_PEOPLE_SUCCESS,
+        payload: res.data.results
+      })
+    )
+    .catch(err => dispatch({
+        type: FETCH_PEOPLE_FAIL,
+        payload: err
+      })
+    )
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,34 @@
-import /* we need our action types here*/ "../actions";
+import
+{
+  FETCH_PEOPLE_START,
+  FETCH_PEOPLE_SUCCESS,
+  FETCH_PEOPLE_FAIL
+} from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  isFetching: false,
+  error: ''
   // Array characters, Boolean fetching, null error.
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCH_PEOPLE_START:
+
+      return {
+        ...state,
+        isFetching: true,
+        error: ''
+      }
+    case FETCH_PEOPLE_SUCCESS:
+
+      return {
+        ...state,
+        characters: action.payload,
+        isFetching: false,
+        error: ''
+      }
     default:
       return state;
   }
